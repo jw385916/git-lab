fcf1861d786407fd8a969e944f12593e98b6b3ca
Jack Wolshire <jw385916@ohio.edu>
2019-01-23 19:43:17 -0500

    compiled code

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jack Wolshire
+ *        Date: 1-23-19
+ * Description: lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) 
+{
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
